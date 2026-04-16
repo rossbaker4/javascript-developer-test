506c7941291ccea9736454a839e5af0dd40d39d2
rossbaker4 <ross.baker4@icloud.com>
2026-04-16 12:53:40 +1000

    getArnieQuotes implementation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,20 @@
 const { httpGet } = require('./mock-http-interface');
 
+const HTTP_STATUS_OK = 200;
+const SUCCESS_KEY = 'Arnie Quote';
+const FAILURE_KEY = 'FAILURE';
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(
+    urls.map(async (url) => {
+      const { status, body } = await httpGet(url);
+      const { message } = JSON.parse(body);
+
+      return status === HTTP_STATUS_OK
+        ? { [SUCCESS_KEY]: message }
+        : { [FAILURE_KEY]: message };
+    })
+  );
 };
 
 module.exports = {
